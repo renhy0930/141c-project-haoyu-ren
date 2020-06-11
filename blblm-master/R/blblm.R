708a2c149b25d41d975b8f8c250211fee0103ead
renhy0930 <59857940+renhy0930@users.noreply.github.com>
2020-06-11 02:38:05 -0700

    d

---
+++ blblm-master/R/blblm.R
@@ -0,0 +1,178 @@
+#' @import purrr
+#' @import stats
+#' @import parallel
+#' @importFrom magrittr %>%
+#' @importFrom utils capture.output
+#' @importFrom utils read.csv
+#' @details
+#' Linear Regression with Little Bag of Bootstraps
+"_PACKAGE"
+
+
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+# from https://github.com/jennybc/googlesheets/blob/master/R/googlesheets.R
+utils::globalVariables(c("."))
+
+
+#' @export
+blblm <- function(formula, data, m = 10, B = 5000, cpu = 1) {
+  if (is.data.frame(data)){
+  data_list <- split_data(data, m)
+  } else {
+    data_list <- map(data, function(x) read.csv(x))
+  }
+  estimates <- map(
+    data_list,
+    ~ lm_each_subsample(formula = formula, data = ., n = nrow(data), B = B, cpu = cpu))
+  res <- list(estimates = estimates, formula = formula)
+
+  class(res) <- "blblm"
+  invisible(res)
+}
+
+#' a function to split dataframe
+#' @param data name of dataset
+#' @param m split to m parts
+#' split data into m parts of approximated equal sizes
+split_data <- function(data, m) {
+  idx <- sample.int(m, nrow(data), replace = TRUE)
+  data %>% split(idx)
+}
+
+#' compute the estimates
+#' @param formula object of formula ex. x~y
+#' @param data name of dataset
+#' @param n sample size
+#' @param B bootstrap size
+#' @param cpu number of cpu
+lm_each_subsample <- function(formula, data, n, B, cpu) {
+  # add parallel
+  cl <- makeCluster(cpu)
+  #replicate(B, lm_each_boot(formula, data, n), simplify = FALSE)
+  clusterExport(cl, c("lm_each_boot", "lm1", "data"))
+  result <- parLapply(cl, 1:B, function(x){
+    lm_each_boot(formula, data, nrow(data))
+  })
+  stopCluster(cl)
+  return(result)
+}
+
+#' compute the regression estimates for a blb dataset
+#' @param formula object of formula
+#' @param data a data set
+#' @param n overall sample size
+lm_each_boot <- function(formula, data, n) {
+  freqs <- rmultinom(1, n, rep(1, nrow(data)))
+  lm1(formula, data, freqs)
+}
+
+#' estimate the regression estimates based on given the number of repetitions
+#' @param formula object of "formula" class
+#' @param data a data set
+#' @param freqs frequecy values
+lm1 <- function(formula, data, freqs) {
+  # drop the original closure of formula,
+  # otherwise the formula will pick a wront variable from the global scope.
+  environment(formula) <- environment()
+  fit <- lm(formula, data, weights = freqs)
+  list(coef = blbcoef(fit), sigma = blbsigma(fit))
+}
+
+#' compute the coefficients from fit
+#' @param fit fitted model
+blbcoef <- function(fit) {
+  coef(fit)
+}
+
+#' compute sigma from fit
+#' @param fit fitted model
+blbsigma <- function(fit) {
+  p <- fit$rank
+  y <- model.extract(fit$model, "response")
+  e <- fitted(fit) - y
+  w <- fit$weights
+  sqrt(sum(w * (e^2)) / (sum(w) - p))
+}
+
+
+#' @export
+#' @method print blblm
+print.blblm <- function(x, ...) {
+  cat("blblm model:", capture.output(x$formula))
+  cat("\n")
+}
+
+
+#' @export
+#' @method sigma blblm
+sigma.blblm <- function(object, confidence = FALSE, level = 0.95, ...) {
+  est <- object$estimates
+  sigma <- mean(map_dbl(est, ~ mean(map_dbl(., "sigma"))))
+  if (confidence) {
+    alpha <- 1 - 0.95
+    limits <- est %>%
+      map_mean(~ quantile(map_dbl(., "sigma"), c(alpha / 2, 1 - alpha / 2))) %>%
+      set_names(NULL)
+    return(c(sigma = sigma, lwr = limits[1], upr = limits[2]))
+  } else {
+    return(sigma)
+  }
+}
+
+#' @export
+#' @method coef blblm
+coef.blblm <- function(object, ...) {
+  est <- object$estimates
+  map_mean(est, ~ map_cbind(., "coef") %>% rowMeans())
+}
+
+
+#' @export
+#' @method confint blblm
+confint.blblm <- function(object, parm = NULL, level = 0.95, ...) {
+  if (is.null(parm)) {
+    parm <- attr(terms(object$formula), "term.labels")
+  }
+  alpha <- 1 - level
+  est <- object$estimates
+  out <- map_rbind(parm, function(p) {
+    map_mean(est, ~ map_dbl(., list("coef", p)) %>% quantile(c(alpha / 2, 1 - alpha / 2)))
+  })
+  if (is.vector(out)) {
+    out <- as.matrix(t(out))
+  }
+  dimnames(out)[[1]] <- parm
+  out
+}
+
+#' @export
+#' @method predict blblm
+predict.blblm <- function(object, new_data, confidence = FALSE, level = 0.95, ...) {
+  est <- object$estimates
+  X <- model.matrix(reformulate(attr(terms(object$formula), "term.labels")), new_data)
+  if (confidence) {
+    map_mean(est, ~ map_cbind(., ~ X %*% .$coef) %>%
+      apply(1, mean_lwr_upr, level = level) %>%
+      t())
+  } else {
+    map_mean(est, ~ map_cbind(., ~ X %*% .$coef) %>% rowMeans())
+  }
+}
+
+
+mean_lwr_upr <- function(x, level = 0.95) {
+  alpha <- 1 - level
+  c(fit = mean(x), quantile(x, c(alpha / 2, 1 - alpha / 2)) %>% set_names(c("lwr", "upr")))
+}
+
+map_mean <- function(.x, .f, ...) {
+  (map(.x, .f, ...) %>% reduce(`+`)) / length(.x)
+}
+
+map_cbind <- function(.x, .f, ...) {
+  map(.x, .f, ...) %>% reduce(cbind)
+}
+
+map_rbind <- function(.x, .f, ...) {
+  map(.x, .f, ...) %>% reduce(rbind)
+}
